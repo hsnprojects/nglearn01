b4968b249c527b539eb79c147ad6446fd579af09
null <null>
2024-04-19 09:03:57 -0500

    Added backtotop button

--- src/app/components/main-body/main-body.component.ts
+++ src/app/components/main-body/main-body.component.ts
@@ -5,9 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './main-body.component.html',
   styleUrls: ['./main-body.component.css']
 })
+
 export class MainBodyComponent {
 
   dynamicBody: string = "<div>test</div>";
+
+  severalLines: number[] = [];
+
+  public constructor() {
+    for (let i = 0; i < 50; i++) {
+      this.severalLines.push(i);
+    }
+  }
 
   // public constructor(){
   //   for(let i=0; i< 30; i++)
